0dade071f00ebf8db1c22c4fa01c9854f57ca885
Safet Salkanović <salkanovicsafet@gmail.com>
2022-02-05 21:41:46 +0100

    Write die component html and css

--- src/App.js
+++ src/App.js
@@ -1,3 +1,20 @@
+import Die from "./components/Die";
+
 export default function App() {
-  return <main>AAAA</main>;
+  return (
+    <main>
+      <div className="dice">
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+      </div>
+    </main>
+  );
 }
